d31ce454b687597c1e8236aa7b5785be3f772a42
Poornima-Joshi <poornima.joshi776@gmail.com>
2024-01-02 23:35:45 +0530

    add fetch

---
+++ app.js
@@ -0,0 +1,18 @@
+const cart = ["shoes","fans","kurta"];
+
+// //call back 
+// createOrder(cart,function(orderId){
+//   proceedToPayment(orderId);
+// });
+// //promises
+// const promise = createOrder(cart);
+// promise.then(function(orderId){
+//   proceedToPayment(orderId);
+// });
+//fetch
+const GitHub = "https://fakestoreapi.com/products";
+const user = fetch(GitHub);
+console.log(user);
+user.then(function(data){
+  console.log(data);
+})
